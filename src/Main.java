a1c14bd9dd65773c456319dfb99f65d2726006be
aleksandr-kuksov <alexku09@mail.ru>
2023-11-15 00:01:53 +0300

    Working on project 19-11-2023

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,16 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+/**
+ * A program that prints <code>Hello world!</code>
+ *
+ * @author aleksandr-kuksov
+ * @version 1.0
+ */
 public class Main {
-    public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
+    /**
+     * Program entry point.
+     *
+     * @param cmdLinesArgs command-line arguments
+     */
+    public static void main(String[] cmdLinesArgs) {
+       System.out.println("Hello world!");
     }
 }
